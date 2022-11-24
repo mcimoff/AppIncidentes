b4d42f76402db825ec8507fecdbe104251495a30
FeliMerlino <felimerlino@gmail.com>
2022-11-23 22:34:27 -0300

    Autorización con token

--- BackEndIncidentes/routes/incidentes.js
+++ BackEndIncidentes/routes/incidentes.js
@@ -9,35 +9,35 @@ router.get('/:id',  async(req, res) => {
   res.json(await controller.getIncidentes(req.params.id));
 });
 
-// router.delete('/borrarIncidente/:id', async(req,res) =>{
-//     const incidente = await data.borrarIncidente(req.params.id);
-//     res.json(incidente);
+router.delete('/borrarIncidente/:id', async(req,res) =>{
+    const incidente = await data.borrarIncidente(req.params.id);
+    res.json(incidente);
+})
+
+// router.delete('/borrarIncidentes', async(req,res) =>{
+//   const incidentes = await controller.borrarIncidentes();
+//   res.json(incidentes)
 // })
 
-// // router.delete('/borrarIncidentes', async(req,res) =>{
-// //   const incidentes = await controller.borrarIncidentes();
-// //   res.json(incidentes)
-// // })
+router.get('/incidenteXID/:id', async(req,res) =>{
+  const incidente = await data.getIncidenteID(parseInt(req.params.id));
+  res.json(incidente);
+})
 
-// router.get('/incidenteXID/:id', async(req,res) =>{
-//   const incidente = await data.getIncidenteID(parseInt(req.params.id));
-//   res.json(incidente);
-// })
+router.post('/nuevoIncidente', async (req, res) => {
+  const incidente = req.body;
+  const result = await data.addIncidente(incidente);
+  res.json(result);
+});
 
-// router.post('/nuevoIncidente', async (req, res) => {
-//   const incidente = req.body;
-//   const result = await data.addIncidente(incidente);
-//   res.json(result);
-// });
+router.get('/obtenerultimoid', async(req,res) =>{
+  res.json(await data.obtenerultimoid());
+})
 
-// router.get('/obtenerultimoid', async(req,res) =>{
-//   res.json(await data.obtenerultimoid());
-// })
+router.get('/incidenteXArea/:area', authUser.checkIfAuthenticated, async(req,res) =>{
 
-// router.get('/incidenteXArea/:area', authUser.checkIfAuthenticated, async(req,res) =>{
-
-//   const incidente = await data.getIncidenteXArea(req.params.area);
-//   res.json(incidente);
-// })
+  const incidente = await data.getIncidenteXArea(req.params.area);
+  res.json(incidente);
+})
 
 module.exports = router;
--- BackEndIncidentes/routes/usuarios.js
+++ BackEndIncidentes/routes/usuarios.js
@@ -3,23 +3,23 @@ var router = express.Router();
 const bodyParser = require('body-parser');
 const data = require('../data/usuarios');
 const check = require('../middleware/authUser')
-//import {checkIfAuthenticated} from '../middleware/authUser';
 
-// router.get('/', check.checkIfAuthenticated, async(req, res) => {
-//   res.json(await data.getUsuarios());
-// });
 
-// router.post('/nuevousuario', async (req, res) => {
-//   const usuario = req.body;
-//   console.log(usuario);
-//   const result = await data.addUsuario(usuario);
-//   res.json(result);
-// });
+router.get('/', check, async(req, res) => {
+  res.json(await data.getUsuarios());
+});
 
-// router.get('/getusuario/:email', async(req,res) =>{
-//   console.log(req.params.email);
-//   const usuario = await data.getUsuarioXCorreo(req.params.email);
-//   res.json(usuario);
-// })
+router.post('/nuevousuario', async (req, res) => {
+  const usuario = req.body;
+  console.log(usuario);
+  const result = await data.addUsuario(usuario);
+  res.json(result);
+});
+
+router.get('/getusuario/:email', check, async(req,res) =>{
+  console.log(req.params.email);
+  const usuario = await data.getUsuarioXCorreo(req.params.email);
+  res.json(usuario);
+})
 
 module.exports = router;
